c09b9b28e61a413345a41ee5f2d9865a0a88251c
hodgesto <hodgesto@osuosl.org>
2017-06-26 18:22:10 +0000

    Added security

--- packer/scripts/common/sshd.sh
+++ packer/scripts/common/sshd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eux
 
 echo "UseDNS no" >> /etc/ssh/sshd_config
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
 echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
 echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
 echo "KbdInteractiveAuthentication no" >> /etc/ssh/sshd_config
