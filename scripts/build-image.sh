2294bf68f54fc31e7960d3abde61b040f01bb361
Lance Albertson <lance@osuosl.org>
2017-06-26 13:25:26 -0700

    OpenStack Mitaka All-in-One image
    This image uses chef to create an all-in-one install of OpenStack for use with
    the osl-openstack cookbook. This allows us to quickly test upgrades from Mitaka
    to Newton.
    
    This setup was pulled in from what we have in test-kitchen primarily.

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -35,8 +35,14 @@ done
 [ -z "$TEMPLATE" ] && echo "Error: Template file not set" && exit 1
 
 DIR_NAME="packer-$(basename -s .json $TEMPLATE)"
+TEMPLATE_NAME="$(basename -s .json $TEMPLATE)"
 IMAGE_NAME=$(grep vm_name $TEMPLATE | awk '{print $2}' | sed -e 's/\"//g' | sed -e 's/,//g')
 set -xe
+if [ -e chef/${TEMPLATE_NAME}/Berksfile ] ; then
+  export BERKSHELF_PATH="chef/berkshelf"
+  rm -rf $BERKSHELF_PATH chef/${TEMPLATE_NAME}/cookbooks chef/${TEMPLATE_NAME}/Berksfile.lock
+  berks vendor -b chef/${TEMPLATE_NAME}/Berksfile chef/${TEMPLATE_NAME}/cookbooks
+fi
 cd packer
 packer build -var "chef_version=$CHEF_VERSION" $(basename $TEMPLATE)
 qemu-img convert -o compat=0.10 -O qcow2 -c ${DIR_NAME}/${IMAGE_NAME}.qcow2 \
