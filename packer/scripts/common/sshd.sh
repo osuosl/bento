1ad622ae8ed2ee340c7e847faa25ae601c19332b
hodgesto <hodgesto@osuosl.org>
2017-06-29 17:23:25 +0000

    modified sshd_config using sed in place of echo

--- packer/scripts/common/sshd.sh
+++ packer/scripts/common/sshd.sh
@@ -3,6 +3,6 @@
 echo "UseDNS no" >> /etc/ssh/sshd_config
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config
 echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
-echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
 echo "KbdInteractiveAuthentication no" >> /etc/ssh/sshd_config
-echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication .*/PasswordAuthentication no/Ig' /etc/ssh/sshd_config
+sed -i 's/ChallengeResponseAuthentication .*/ChallengeResponseAuthentication no/Ig' /etc/ssh/sshd_config
